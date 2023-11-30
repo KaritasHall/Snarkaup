c69dc258afb0dd21d4f68a3f4e2df4a3b1716d68
Karitas <karitashall@gmail.com>
2023-11-30 17:13:06 +0000

    remove unwanted fragment

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -8,12 +8,10 @@ export default function Page({ params }: { params: { id: string } }) {
   });
 
   return (
-    <>
-      <SectionContainer>
-        <h2>This is a slug!</h2>
-        <p className="text-black">{params.id}</p>
-        <p className="text-black">{product?.title}</p>
-      </SectionContainer>
-    </>
+    <SectionContainer>
+      <h2>This is a slug!</h2>
+      <p className="text-black">{params.id}</p>
+      <p className="text-black">{product?.title}</p>
+    </SectionContainer>
   );
 }
