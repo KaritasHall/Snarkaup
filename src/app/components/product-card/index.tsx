6d033fe3d216e254dc87f4de0e54d05bb2f3e4bc
Karitas <karitashall@gmail.com>
2023-12-08 09:05:20 +0000

    Adds recoil

--- src/app/components/product-card/index.tsx
+++ src/app/components/product-card/index.tsx
@@ -1,15 +1,14 @@
 import { Product } from "@/app/hooks/useProducts";
 import Image from "next/image";
 import Link from "next/link";
-
 interface ProductCardProps {
   product: Product;
 }
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <div className="w-168 lg:w-255 grid grid-rows-[auto_1fr] gap-8 rounded-md bg-white px-8 py-12 shadow-lg lg:gap-12">
-      <div className="h-160 md:h-168 lg:h-255 w-full lg:pb-4">
+    <div className="grid w-168 grid-rows-[auto_1fr] gap-8 rounded-md bg-white px-8 py-12 shadow-lg lg:w-255 lg:gap-12">
+      <div className="h-160 w-full md:h-168 lg:h-255 lg:pb-4">
         <Link href={`/products/${product.id}`}>
           <Image
             src={product.image}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,6 +3,7 @@
 import { ApolloProvider } from "@apollo/client";
 import { DatoClient } from "./dato/apollo-client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
 
 export const queryClient = new QueryClient();
 
@@ -10,8 +11,10 @@ export const queryClient = new QueryClient();
 // and the Apollo Client to its children's children.
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ApolloProvider client={DatoClient}>{children}</ApolloProvider>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ApolloProvider client={DatoClient}>{children}</ApolloProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 };
