919093c9de9255d31e3b7236ada7dcd09386e303
Karitas <karitashall@gmail.com>
2023-12-12 20:29:01 +0000

    Clean up test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { GET_FRONT_PAGE } from "./dato/front-page-query";
 import { useProducts } from "./hooks/useProducts";
 import SectionContainer from "./components/section-container";
 import ProductCard from "./components/product-card";
-import LinkComponent from "./components/link-component";
 
 export default function Home() {
   const { loading, error, data } = useQuery(GET_FRONT_PAGE);
@@ -14,14 +13,6 @@ export default function Home() {
   return (
     <>
       <SectionContainer>
-        <div className="p-20">
-          <LinkComponent
-            label="Collections"
-            href="/shop"
-            showArrow
-            color="black"
-          />
-        </div>
         <div className="grid grid-cols-1 gap-[64px] lg:grid-cols-4">
           {products?.map((product) => (
             <ProductCard key={product?.id} product={product} />
