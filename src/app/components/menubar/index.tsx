8f5eb6e315bfdbe45b40bf9494e288a2dba485c9
Karitas <karitashall@gmail.com>
2024-02-07 12:08:00 +0000

    Adds styling to navbar + aria label to button component (#23)

--- src/app/components/menubar/index.tsx
+++ src/app/components/menubar/index.tsx
@@ -1,7 +1,7 @@
 // Menubar is only rendered on desktop
 const Menubar = () => {
   return (
-    <nav className="hidden w-full outline outline-black lg:block lg:h-[51px]">
+    <nav className="hidden w-full bg-blue outline outline-black lg:block lg:h-[51px]">
       <h1>Menubar</h1>
     </nav>
   );
--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,16 +1,22 @@
-import Menubar from "../menubar";
 import SearchBar from "../searchbar";
+import { ShoppingBagIcon } from "../icons/shopping-bag-icon";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className="px-fluid-x pb-10 lg:pb-30">
-      <nav className="flex h-fit w-full justify-between gap-24 py-16 lg:h-88 lg:pb-18 lg:pt-24">
-        <h1>Logo</h1>
-
-        <SearchBar placeholder="Search" />
-        <p>Cart</p>
+      <nav className="flex h-fit w-full justify-between py-16 lg:h-88 lg:pb-18 lg:pt-24">
+        <div className="flex w-1/2 gap-18">
+          <Link href="/" aria-label="Link to home page">
+            <h1 className="">Logo</h1>
+          </Link>
+          <h2 className="">Shop</h2>
+        </div>
+        <div className="flex w-1/2 items-center gap-18">
+          <SearchBar placeholder="Search" />
+          <ShoppingBagIcon />
+        </div>
       </nav>
-      {/* <Menubar /> */}
     </div>
   );
 };
